467cb12e4b656571dbcdbe987e60267a48d64ff9
2Bienvenu <ishimwesibomanabienvenu@gmail.com>
2022-12-22 18:21:11 +0200

    form validation

---
+++ script.js
@@ -0,0 +1,16 @@
+const handleSubmit = (event) => {
+  let email = document.querySelector(".email").value;
+  let message = document.querySelector(".message").value;
+  let names = document.querySelector(".names").value;
+  event.preventDefault();
+
+  if (
+    /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+  ) {
+    alert(
+      `HELLO ${names} ,thank you for your Message, We will contact you late on your email: ${email},Thank you!!!`
+    );
+  } else {
+    alert(`${email} is an invalid email! Please try again!!!`);
+  }
+};
