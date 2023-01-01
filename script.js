190597d167d8032c4bac59b0c56df0f4b47179c4
Bien-venu <111735309+Bien-venu@users.noreply.github.com>
2023-01-01 13:51:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ const handleSubmit = (event) => {
   const names = document.querySelector(".names").value;
   event.preventDefault();
 
-  if (
-    /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
-  ) {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
     alert(
       `HELLO ${names}!, thank you for your Message, We will contact you late on your email: ${email},Thank you!!!`
     );
@@ -15,78 +13,76 @@ const handleSubmit = (event) => {
   }
 };
 
-// 
-
-
+//
 
 const getBlogs = () => {
   let result = localStorage.getItem("test");
-  let date= new Date();
-  
+  let date = new Date();
+  let year = date.getFullYear();
+  let month = date.toLocaleString("default", { month: "short" });
+  let day = date.getDate();
+
   let fromStorage = localStorage.getItem("blogs");
   let blogsResult = JSON.parse(fromStorage);
-  console.log(blogsResult)
-  if (blogsResult){
-    document.querySelector('.cc').innerHTML =blogsResult.map((item) =>(
-      `<div class="comments">
-                <div class="comment-img">
-                  <img src="./imgs/comment image.jpg" alt="">
+  console.log(blogsResult);
+  if (blogsResult) {
+    document.querySelector(".mainwork").innerHTML = blogsResult.map(
+      (item) => (`
+    <label for="blogO">
+              <div class="mainleft blogO">
+                <a class="imageo"><img src=${item.image} alt=""></a>
+                <div class="under">
+                  <div class="date">${month} ${day}, ${year}</div>
+                  <h3>${item.title}</h3>
+                  <span>${item.body}</span>
                 </div>
-                <div class="comment-message">
-                  <div class="commenter">${item.name}</div>
-                  <div class="comment-date">${date}</div>
-                  <div class="commente">
-                  ${item.comment}
-                  </div>
-                </div>                
-              </div>`)
-  )
-  document.querySelector('.number').innerHTML= blogsResult.length;
+              </div>
+            </label>`)
+    );
+    document.querySelector(".number").innerHTML = blogsResult.length;
   }
-  
-}
+};
 
-const handle=(e)=>{
+const handle = (e) => {
   e.preventDefault();
-  let name = document.querySelector('.comment-names').value;
-  let email = document.querySelector('.comment-email').value;
-  let comment = document.querySelector('.comment-input').value;
- 
-  if(localStorage.getItem("blogs") == null){
-  let array = [];
+  let title = document.querySelector(".blog-title").value;
+  let image = document.querySelector(".blog-image").value;
+  let body = document.querySelector(".blog-body").value;
+
+  if (localStorage.getItem("blogs") == null) {
+    let array = [];
     const blog = {
-      id:1,
-    name,
-    email,
-    comment
-  }
-  array.push(blog);
-  localStorage.setItem("blogs", JSON.stringify(array));
-  window.alert("Blog added successfuly");
-    getBlogs();
-  } else {
-    
-    let array = JSON.parse(localStorage.getItem("blogs"));
-    const blog = {
-      id:(array.length+1),
-    name,
-    email,
-    comment
-  }
+      id: 1,
+      title,
+      image,
+      body
+    };
     array.push(blog);
     localStorage.setItem("blogs", JSON.stringify(array));
-  window.alert("Blog added successfuly");
+    window.alert("Blog added successfuly");
+    getBlogs();
+  } else {
+    let array = JSON.parse(localStorage.getItem("blogs"));
+    const blog = {
+      id: array.length + 1,
+      title,
+      image,
+      body
+    };
+    array.push(blog);
+    localStorage.setItem("blogs", JSON.stringify(array));
+    window.alert("Blog added successfuly");
     getBlogs();
   }
-}
+};
 
 const editBlog = (blogId) => {
-  console.log(blo)
+  console.log(blo);
   let array = JSON.parse(localStorage.getItem("blogs"));
-  array.map((item)=>{
-    if(blogId == item.id){
-      item.name = "New Value"
+  array.map((item) => {
+    if (blogId === item.id) {
+      item.title = "New Value";
     }
-  })
+  });
   localStorage.setItem("blogs", JSON.stringify(array));
-}
+};
